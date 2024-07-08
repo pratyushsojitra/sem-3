509bc2ce0429ab6d9badc54b8409d3b54c5d6245
pratyushsojitra <143513788+pratyushsojitra@users.noreply.github.com>
2024-07-08 16:47:09 +0530

    Add files via upload

---
+++ dbms/Lab-4.sql
@@ -0,0 +1,36 @@
+--Alter Operation
+--Part – A:
+--Use Deposit table of lab-1.
+--DEPOSIT
+--Column_Name DataType
+--ACTNO INT
+--CNAME VARCHAR(50)
+--BNAME VARCHAR(50)
+--AMOUNT DECIMAL(8,2)
+--ADATE DATETIME
+--From the above given tables perform the following queries (ALTER Operation):
+--1. Add two more columns City VARCHAR (20) and Pincode INT.
+--2. Change the size of CNAME column from VARCHAR (50) to VARCHAR (35).
+--3. Change the data type DECIMAL to INT in amount Column.
+--4. Rename Column ActNo to ANO.
+--5. Delete Column City from the DEPOSIT table.
+--6. Change name of table DEPOSIT to DEPOSIT_DETAIL.
+--Part – B:
+--1. Rename Column ADATE to AOPENDATE OF DEPOSIT_DETAIL table.
+--2. Delete Column AOPENDATE from the DEPOSIT_DETAIL table.
+--3. Rename Column CNAME to CustomerName.
+--Part – C:
+--Create following table using query according to the definition.
+--STUDENT_DETAIL
+--Column_Name DataType
+--Enrollment_No VARCHAR(20)
+--Name VARCHAR(25)
+--CPI DECIMAL(5,2)
+--Birthdate DATETIME
+--From the above given tables perform the following queries (ALTER Operation):
+--1. Add two more columns City VARCHAR (20) (Not null) and Backlog INT (Null).
+--2. Change the size of NAME column of student_detail from VARCHAR (25) to VARCHAR (35).
+--3. Change the data type DECIMAL to INT in CPI Column.
+--4. Rename Column Enrollment_No to ENO.
+--5. Delete Column City from the student_detail table.
+--6. Change name of table student_detail to STUDENT_MASTER.
