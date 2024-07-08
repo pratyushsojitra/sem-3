3dc3c3f5dcd13d1ffc386c2b578099a649660707
pratyushsojitra <143513788+pratyushsojitra@users.noreply.github.com>
2024-07-08 17:08:42 +0530

    Update ABForm.java

--- ds/Lab-7/ABForm.java
+++ ds/Lab-7/ABForm.java
@@ -1,21 +1,32 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class ABForm {
     public static void main(String[] args) {
-        MyStack<Character> sa = new MyStack<>();
-        MyStack<Character> sb = new MyStack<>();
+        Stack<Character> s1 = new Stack<>();
+        Stack<Character> s2 = new Stack<>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter string (contains a and b only) : ");
+        System.out.println("Enter string which have only a and b : ");
         String str = sc.next();
         for(char c : str.toCharArray()) {
             if(c=='a') {
-                sa.push(c);
+                s1.push(c);
             }
             else {
-                sb.push(c);
+                s2.push(c);
             }
         }
-        System.out.println("a = "+sa.size()+"\nb = "+sb.size());
+        int size= str.length();
+        if ((size%2)!=0) {
+            System.out.println("this is false");
+        }
+        else{
+            if (s1.size()==s2.size()) {
+                System.out.println("this is true");
+            }
+            else{
+                System.out.println("this is false");
+            }
+        }
         sc.close();
     }
 }
